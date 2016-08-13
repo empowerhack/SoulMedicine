a2e1ecaef6bbe580125f84f01ab154d94520344a
HisashiQ <quinnkoike@gmail.com>
2016-08-13 16:29:49 +0100

    fixed precompile issue

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,4 @@ Rails.application.routes.draw do
     get '/logout' => 'sessions#destroy'
     match ':controller(/:action(/:id))', :via => [:get, :post, :patch]
   end
-
-  constraints SubdomainConstraint do
-    get "/", to: 'users#index', constraints: { subdomain: ['soulmedicine-staging', 'soulmedicine'] }, via: [:get, :post, :put, :patch, :delete]
-  end
 end
