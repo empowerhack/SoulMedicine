b76b5ac64da73c8083ba870c2c9ce4c288bc5409
Fahad <iqbal.fahad90+cloud9@gmail.com>
2016-08-24 12:13:19 +0000

    updated Course admin page

--- app/admin/course.rb
+++ app/admin/course.rb
@@ -5,19 +5,18 @@ ActiveAdmin.register Course do
     permit_params :name, :description, :order, :is_active
     permit_params subject_matters_attributes: [:_destroy, :id, :name, :description, :order]
     
+    remove_filter :lessons, :user, :user_courses
+    
+    controller do
+        def scoped_collection
+            super.includes :subject_matters, :lessons
+        end
+    end
+    
     index do
         selectable_column
         column :name
         column :description
-        # column do |course|
-        #     if course.subject_matters.exists?
-        #         table_for course.subject_matters.order("`order` ASC") do
-        #           column do |sm|
-        #             link_to sm.name, admin_subject_matter_path(sm)
-        #           end
-        #         end
-        #     end
-        #   end
         column :order
         column :is_active
         column "Subjects" do |cc|
@@ -30,6 +29,37 @@ ActiveAdmin.register Course do
             end
           end
         actions
+    end
+    
+    show do
+        columns do
+            column max_width: "35%" do
+                panel course.name do
+                    text_node course.description
+                    hr
+                    text_node "Order: #{course.order}"
+                    hr
+                    h5 "Active?"
+                    status_tag course.is_active
+                end
+            end
+            column max_width: "65%" do
+                panel "Subject Matters" do
+                    table_for course.subject_matters do
+                        column "Name" do |sm|
+                            link_to sm.name, admin_subject_matter_path(sm)
+                        end
+                        column "Lessons" do |sm|
+                            ul {
+                                sm.lesson.each do |l|
+                                    li link_to l.name, admin_lesson_path(l)
+                                end
+                            }
+                        end
+                    end
+                end
+            end
+        end
     end
     
     form do |f|
