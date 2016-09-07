9b5a0cad15d18311bb5b9d59d237d25eddc0a3fd
Fahad <iqbal.fahad90+git@gmail.com>
2016-09-07 15:57:20 +0100

    added new model tests

--- test/models/subject_matter_test.rb
+++ test/models/subject_matter_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class SubjectMatterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "subject matter attirbutes must not be empty" do
+    sm = SubjectMatter.new
+    assert sm.invalid?, "subject matter is invalid"
+    assert sm.errors[:name].any?, "subject matter has no name"
+    assert sm.errors[:description].any?, "subject matter has no description"
+    assert sm.errors[:order].any?, "subject matter has no order"
+    assert sm.errors[:course_id].any?, "subject matter has no course"
+    assert_not sm.save, "subject matter saved with no attributes provided"
+  end
+  test "two subject matters cannot exist in the same course with the same name" do
+    sm = SubjectMatter.where(:name => "Subject 1").first
+    dup = SubjectMatter.new(:name => sm.name, :course_id => sm.course_id, :description => sm.description, :order => 2)
+    assert_equal "Subject 1", sm.name
+    assert_not dup.save, "duplicate subject matter saved"
+  end
 end
