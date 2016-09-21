44b672e0e19ac9b24b6fc6fbc49726947f3da881
HisashiQ <quinnkoike@gmail.com>
2016-09-21 13:57:40 +0100

    Test for Sidetiq schedular

--- app/workers/test_worker.rb
+++ app/workers/test_worker.rb
@@ -1,10 +1,12 @@
 class TestWorker
   include Sidekiq::Worker
-  def perform(name)
-      logger.info "Name: #{name} -- Time: #{Time.now}"
-    file = File.join(Rails.root, 'tmp', 'test.txt')
-    File.open(file, "w+") do |f|
-        f.write("Name: #{name} -- Time: #{Time.now}")
-    end
+  include Sidetiq::Schedulable
+
+  recurrence do
+	  hourly.minute_of_hour(30,31,32,33,34,35,36,37,38,39,40)
   end
+
+  def perform
+      puts "Name: quinn -- Time: #{Time.now}"
+    end
 end
