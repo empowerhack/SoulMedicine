3b78c365e6235da5af933e22b02dd1af9af764e7
Fahad <iqbal.fahad90+cloud9@gmail.com>
2016-09-23 09:36:00 +0000

    fixed a navbar issue on mobile

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,13 @@
 
 $( document ).on('ready page:change', function() {
+    console.log('Page ready');
     $('.message .close').on('click', function() {
       $(this).closest('.message').transition('fade');
     });
       
     $('.ui.sidebar').sidebar('attach events', '.toc.item');
     $('.toc.item').on('click', function() {
+      console.log('Sidebar toggled');
      $('.ui.sidebar').sidebar('toggle');
     });
     
