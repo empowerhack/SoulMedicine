c4fec78359e27cf0430eda9c7935ac45eb5c3a2e
Fahad Iqbal <iqbal.fahad90@gmail.com>
2016-09-29 10:20:00 +0000

    added validation to LessonCompletion model

--- app/models/lesson_completion.rb
+++ app/models/lesson_completion.rb
@@ -1,6 +1,7 @@
 class LessonCompletion < ActiveRecord::Base
 
   validates_presence_of [:user_id, :lesson_id, :subject_matter_id, :course_id]
+  validates_uniqueness_of :lesson_id, :scope => :user_id, on: :create, message: "must be unique"
   
   belongs_to :lesson
   belongs_to :subject_matter
